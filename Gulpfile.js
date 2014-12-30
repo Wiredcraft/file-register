1aad7f5289384356929217f729fd0001b2e81d3a
Makara Wang <makara15@gmail.com>
2014-12-30 17:50:01 +0800

    The very basic files.

---
+++ Gulpfile.js
@@ -0,0 +1,64 @@
+process.env.NODE_ENV = 'test';
+process.env.DEBUG = 'carcass:*';
+require('should');
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+var coffeelint = require('gulp-coffeelint');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var coverage = require('gulp-coverage');
+
+// Files.
+var src = 'src/**/*.coffee';
+var tests = 'test/*.mocha.js';
+
+// Compile coffee scripts.
+gulp.task('coffee', function() {
+    return gulp.src(src)
+        .pipe(coffee({
+            bare: true
+        }).on('error', gutil.log))
+        .pipe(gulp.dest('./'));
+});
+
+// Lint (or hint as an alias).
+gulp.task('lint', ['coffeelint', 'jshint']);
+gulp.task('hint', ['coffeelint', 'jshint']);
+gulp.task('coffeelint', function() {
+    return gulp.src(src)
+        .pipe(coffeelint())
+        .pipe(coffeelint.reporter());
+});
+gulp.task('jshint', function() {
+    return gulp.src(['Gulpfile.js', tests])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Run tests.
+gulp.task('mocha', ['coffee'], function() {
+    return gulp.src(tests)
+        .pipe(coverage.instrument({
+            pattern: ['index.js', 'lib/**/*.js'],
+            debugDirectory: 'test/debug'
+        }))
+        .pipe(mocha({
+            reporter: 'spec'
+        }))
+        .pipe(coverage.report({
+            outFile: 'test/coverage.html'
+        }));
+});
+
+// Run benchmark.
+gulp.task('bm', ['coffee'], function() {
+    return gulp.src('benchmark/*.js')
+        .pipe(mocha({
+            timeout: 120000,
+            reporter: 'spec'
+        }));
+});
+
+gulp.task('default', ['coffeelint', 'jshint', 'coffee', 'mocha']);
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-file-register
-=============
+# File Register
 
 (Node.js) The way we organize code: break into files and folders and map to an object.
+
+Originally part of [Carcass](https://github.com/Wiredcraft/carcass).
+
+## How to use
+
+TODO

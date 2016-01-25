de4522b8e18147e73916ea95006ab1eec3088a3b
Makara Wang <makara15@gmail.com>
2016-01-25 19:27:44 +0800

    Dropped coffee.

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-var Register, mixable, path;
-
-path = require('path');
-
-mixable = require('mixable-object');
-
+var path = require('path');
+var mixable = require('mixable-object');
 
 /**
  * A simple implementation that does nothing but only register files.
  */
+function Register() {
+  if (!(this instanceof Register)) return new Register();
+}
 
-module.exports = Register = function() {
-  if (!(this instanceof Register)) {
-    return new Register();
-  }
-};
-
+// Export the original prototype.
 Register.proto = require('./lib/proto');
 
+// Mixin.
 mixable(Register);
-
 Register.prototype.mixin(Register.proto);
+
+module.exports = Register;
